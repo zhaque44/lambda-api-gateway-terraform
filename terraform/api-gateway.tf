1ebb70d72315568fce83db77ae594c002cea99cf
Zubair Haque <haque.zubair@gmail.com>
2023-06-22 14:35:25 -0500

    Update api-gateway.tf

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -2,34 +2,33 @@ resource "aws_api_gateway_rest_api" "api" {
   name        = "api"
   description = "API Gateway"
 }
-
-resource "aws_apigatewayv2_stage" "dev" {
-  api_id = aws_apigatewayv2_api.main.id
-
-  name        = "dev"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.main_api_gw.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
+-- this is the policy for the API Gateway, this policy is used to allow access to the API Gateway from only specific IP addresses, which means that only these servers can access the API Gateway
+resource "aws_api_gateway_rest_api_policy" "example" { 
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Deny",
+        "Principal": "*",
+        "Action": "execute-api:Invoke",
+        "Resource": "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*/*/*",
+        "Condition": {
+          "NotIpAddress": {
+            "aws:SourceIp": [
+              "12.11.117.12/32",
+              "44.245.128.80/32",
+              "199.188.255.126/32"
+            ]
+          }
+        }
+      },
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "execute-api:Invoke",
+        "Resource": "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.edify-api.id}/*/*/*"
       }
-    )
-  }
-}
-
-resource "aws_cloudwatch_log_group" "main_api_gw" {
-  name = "/aws/api-gw/${aws_apigatewayv2_api.main.name}"
-
-  retention_in_days = 30
+    ]
+  })
 }
