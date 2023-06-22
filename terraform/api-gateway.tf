a5603e84a7bec8ad14d43ffa8e6d70f73f0502d9
Zubair Haque <haque.zubair@gmail.com>
2023-06-22 14:52:55 -0500

    Update api-gateway.tf

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -44,3 +44,12 @@ resource "aws_api_gateway_resource" "payment" {
   parent_id   = aws_api_gateway_resource.v1.id
   path_part   = "payment"
 }
+-- this is the integration between the API Gateway and the Lambda function, which means that the API Gateway will call the Lambda function when a request is made to the API Gateway
+resource "aws_api_gateway_integration" "proxy" { 
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = "ANY"
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.handler.invoke_arn
+}
