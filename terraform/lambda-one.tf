2e23846270c4811a2f50f1e61b83cae9b0d967b0
Zubair Haque <haque.zubair@gmail.com>
2023-06-22 21:50:17 -0500

    Update lambda-one.tf

--- terraform/lambda-one.tf
+++ terraform/lambda-one.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "handler_lambda_exec" {
-  name = "handler-lambda"
+resource "aws_iam_role" "lambda_one_exec" {
+  name = "lambda-one"
 
   assume_role_policy = <<POLICY
 {
@@ -17,39 +17,42 @@ resource "aws_iam_role" "handler_lambda_exec" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "handler_lambda_policy" {
-  role       = aws_iam_role.handler_lambda_exec.name
+resource "aws_iam_role_policy_attachment" "lambda_one_policy" {
+  role       = aws_iam_role.lambda_one_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "handler" {
-  function_name = "handler"
+resource "aws_lambda_function" "lambda-one" {
+  function_name = "lambda-one"
+  role          = aws_iam_role.lambda_one_exec.arn
+  image_uri     = "<account-id>.dkr.ecr.<region>.amazonaws.com/lambda-one:latest"
+  package_type  = "Image"
+  timeout       = 60
+  memory_size   = 512
+  publish       = true
 
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.handler.key
+  environment {
+    variables = {
+      "MQ_HOST"                     = "MQ_HOST",
+      "AUTH_TOKEN"                  = "AUTH_TOKEN",
+      "MQ_USERNAME"                 = "MQ_USERNAME",
+      "MQ_PASSWORD"                 = "MQ_PASSWORD",
+      "LAMBDA_NET_SERIALIZER_DEBUG" = true
+    }
+  }
 
-  runtime = "nodejs16.x"
-  handler = "function.handler"
+  image_config {
+    working_directory = "/var/task"
+    command = ["index.lambda-one"]
+  }
 
-  source_code_hash = data.archive_file.handler.output_base64sha256
-
-  role = aws_iam_role.handler_lambda_exec.arn
+  depends_on = [
+    aws_mq_broker.edify-broker,
+  ]
 }
 
-# cloudwatch log group
-resource "aws_cloudwatch_log_group" "handler_lambda" {
-  name = "/aws/lambda/${aws_lambda_function.handler.function_name}"
+resource "aws_cloudwatch_log_group" "lambda-one" {
+  name              = "/aws/lambda/lambda-one"
+  retention_in_days = 10
 }
 
-data "archive_file" "handler" {
-  type        = "zip"
-  source_dir  = "../${path.module}/handler"
-  output_path = "../${path.module}/handler.zip"
-}
-
-resource "aws_s3_object" "handler" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "handler.zip"
-  source = data.archive_file.handler.output_path
-  etag   = filemd5(data.archive_file.handler.output_path)
-}
