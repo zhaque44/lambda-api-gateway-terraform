b35d2509e7831f0a87df9846d83fbfcba603347b
Zubair Haque <haque.zubair@gmail.com>
2023-06-22 21:37:51 -0500

    Update api-gateway.tf

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -2,7 +2,7 @@ resource "aws_api_gateway_rest_api" "api" {
   name        = "api"
   description = "API Gateway"
 }
--- this is the policy for the API Gateway, this policy is used to allow access to the API Gateway from only specific IP addresses, which means that only these servers can access the API Gateway
+-- # this is the IP address of the Edify servers, which means that only theese IPs can access the API Gateway
 resource "aws_api_gateway_rest_api_policy" "example" { 
   rest_api_id = aws_api_gateway_rest_api.api.id
   policy = jsonencode({
@@ -14,11 +14,11 @@ resource "aws_api_gateway_rest_api_policy" "example" {
         "Action": "execute-api:Invoke",
         "Resource": "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*/*/*",
         "Condition": {
-          "NotIpAddress": {
+          "NotIpAddress": { 
             "aws:SourceIp": [
-              "12.11.117.12/32",
-              "44.245.128.80/32",
-              "199.188.255.126/32"
+              "52.11.108.82/32",
+              "44.280.128.40/32",
+              "199.188.244.128/32"
             ]
           }
         }
@@ -32,41 +32,124 @@ resource "aws_api_gateway_rest_api_policy" "example" {
     ]
   })
 }
--- this is the root resource, which means that the API Gateway will be accessible at https://api.endpoint/v1
+
 resource "aws_api_gateway_resource" "v1" { 
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "v1"
 }
- -- this is the payment resource, which means that the API Gateway will be accessible at https://api.endpoint/v1/payment
+
 resource "aws_api_gateway_resource" "payment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.v1.id
   path_part   = "payment"
 }
--- this is the integration between the API Gateway and the Lambda function, which means that the API Gateway will call the Lambda function when a request is made to the API Gateway
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.payment.id
+  path_part   = "{proxy+}"
+}
+
 resource "aws_api_gateway_integration" "proxy" { 
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.proxy.id
   http_method             = "ANY"
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri                     = aws_lambda_function.handler.invoke_arn
+  uri                     = aws_lambda_function.lambda-one.invoke_arn
 }
--- # this is the method that is used to call the lambda function, which means that the lambda function is called when the API Gateway receives a request
-resource "aws_api_gateway_method" "proxy" { 
+
+resource "aws_api_gateway_method" "proxy" {
   rest_api_id      = aws_api_gateway_rest_api.api.id
   resource_id      = aws_api_gateway_resource.proxy.id
-  http_method      = "ANY" -- the method is ANY because the API Gateway can receive any type of request, such as GET, POST, PUT, DELETE, etc.
+  http_method      = "ANY" 
   authorization    = "NONE"
   api_key_required = false
   request_parameters = {
     "method.request.path.proxy" = true
   }
 }
--- # this is the resource for the customer endpoint, which will look like https://api/v1/customer
+
 resource "aws_api_gateway_resource" "customer" { 
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.v1.id
   path_part   = "customer"
+}
+-- This is the second proxy resource, that is used for the customer endpoint, # this is the resource for the customer endpoint
+resource "aws_api_gateway_resource" "proxy2" { 
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.customer.id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_integration" "proxy2" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.proxy2.id
+  http_method             = "ANY"
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.lambda-two.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy2" {
+  rest_api_id      = aws_api_gateway_rest_api.api.id
+  resource_id      = aws_api_gateway_resource.proxy2.id
+  http_method      = "ANY"
+  authorization    = "NONE"
+  api_key_required = false
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_lambda_permission" "lambda-one" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda-one.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "lambda-two" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda-two.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_cloudwatch_log_group" "api" {
+  name = "/aws/api-gateway/api"
+  retention_in_days = 30
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_api_gateway_stage" "gateway_api_stage" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "dev"
+  variables = {
+    "lambdaARN" = aws_lambda_function.lambda-one.invoke_arn,
+    "lambdaARN2" = aws_lambda_function.lambda-two.invoke_arn
+  }
+  deployment_id = aws_api_gateway_deployment.gateway_api_deployment.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_api_gateway_deployment.gateway_api_deployment]
+}
+
+resource "aws_api_gateway_deployment" "gateway_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "development"
+  variables = {
+    "lambdaARN" = aws_lambda_function.lambda-one.invoke_arn,
+    "lambdaARN2" = aws_lambda_function.lambda-two.invoke_arn
+  }
+  depends_on = [aws_api_gateway_integration.proxy, aws_api_gateway_integration.proxy2]
 }
